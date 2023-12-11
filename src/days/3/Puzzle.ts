dd549e77fe7589729db76928eb3e4205d896e923
villagrat <elsainetedelautrec@gmail.com>
2023-12-11 00:50:16 -0300

    Day 3 Problem 1

--- src/days/3/Puzzle.ts
+++ src/days/3/Puzzle.ts
@@ -1,6 +1,4 @@
 const first = (input: string) => {
-  // let startIdx = 0;
-  let result = 0;
   /*
       --- Info ---
       - The puzzle input is a visual representation of the engine
@@ -35,108 +33,69 @@ const first = (input: string) => {
       and their sum is 4361.
   */
   /*
-      Build a matrix of the numbers in the line
-      It will be a NxM matrix where N is the number of lines and M is the length of the line
-      Parsed possible parts in the matrix will include:
-      - value
-      - line n° 
-      - coordinates in line, eg: [0,2]
-      
-      Parsed symbols in the matrix will include:
-      - value
-      - line n°
-      - coordinates in line, eg: [3]
+    1. Split the input into lines
+    2. For each line, replace the periods with spaces
+    3. For each line, find the numbers
+    4. For each number, check if it is adjacent to a symbol in any of the 8 directions
+    5. If it is, add it to the sum
   */
-  const matrix: {
-    value: string;
-    lineNumber: number;
-    coordinates: number[];
-    shouldCheckIfSum: boolean;
-  }[] = [];
-  let lineNumber = 0;
-  let startIdx = 0;
-  for (let i = 0; i < input.length; i++) {
-    if (input[i] === "\n") {
-      const line = input.slice(startIdx, i);
-      const parts = [];
-      const symbols = [];
+  const matrix = input.split("\n");
+  let result = 0;
 
-      let match;
-      const numberRegex = /\d+/g;
-      while ((match = numberRegex.exec(line))) {
-        parts.push({
-          value: match[0],
-          lineNumber,
-          coordinates: [match.index, numberRegex.lastIndex - 1],
-          shouldCheckIfSum: true,
-        });
-      }
-
-      const symbolRegex = /[^\d\.]/g;
-      while ((match = symbolRegex.exec(line))) {
-        symbols.push({
-          value: match[0],
-          lineNumber,
-          coordinates: [match.index],
-          shouldCheckIfSum: false,
-        });
-      }
-
-      matrix.push(...parts, ...symbols);
-      lineNumber++;
-      startIdx = i + 1;
-    }
-  }
-
-  // Sort the matrix by line number and then by the first coordinate
-  matrix.sort((a, b) => {
-    if (a.lineNumber !== b.lineNumber) {
-      return a.lineNumber - b.lineNumber;
-    }
-    return a.coordinates[0] - b.coordinates[0];
-  });
-
+  console.log("");
   console.log("matrix: ", matrix);
-  // Iterate over the matrix and check if each symbol has a part in its adjacent cells
+  console.log("");
+
   for (let i = 0; i < matrix.length; i++) {
-    const item = matrix[i];
-    if (!item.shouldCheckIfSum) {
-      // Check the eight possible adjacent cells
-      const adjacentCells = [
-        [item.lineNumber - 1, item.coordinates[0] - 1],
-        [item.lineNumber - 1, item.coordinates[0]],
-        [item.lineNumber - 1, item.coordinates[0] + 1],
-        [item.lineNumber, item.coordinates[0] - 1],
-        [item.lineNumber, item.coordinates[0] + 1],
-        [item.lineNumber + 1, item.coordinates[0] - 1],
-        [item.lineNumber + 1, item.coordinates[0]],
-        [item.lineNumber + 1, item.coordinates[0] + 1],
-      ];
-      for (let j = 0; j < matrix.length; j++) {
-        if (matrix[j].shouldCheckIfSum) {
-          const partStartCoordinate = matrix[j].coordinates[0];
-          const partEndCoordinate = matrix[j].coordinates[1];
-          if (
-            adjacentCells.some(
-              (cell) =>
-                cell[0] === matrix[j].lineNumber &&
-                cell[1] >= partStartCoordinate &&
-                cell[1] <= partEndCoordinate
-            )
-          ) {
-            result += Number(matrix[j].value);
-            break;
-          }
+    const numbers = matrix[i].replace(/\./g, " ");
+    console.log("");
+    console.log("for i: ", i);
+    console.log("numbers - not dots: ", numbers);
+    console.log("");
+    const regex = /\d+/g;
+    let match;
+    while ((match = regex.exec(numbers)) !== null) {
+      for (let j = match.index; j < match.index + match[0].length; j++) {
+        console.log("");
+        console.log(
+          "These are the cells surrounding cell with value: ",
+          match[0]
+        );
+        console.log("matrix[i][j]: ", matrix[i][j]);
+        console.log("I: ", i);
+        console.log("J: ", j);
+        console.log("match.index: ", match.index);
+        console.log("match[0].length: ", match[0].length);
+        console.log("");
+
+        const surrounding = [
+          (matrix[i - 1] ?? "")[j - 1] ?? ".",
+          (matrix[i - 1] ?? "")[j] ?? ".",
+          (matrix[i - 1] ?? "")[j + 1] ?? ".",
+          (matrix[i] ?? "")[j - 1] ?? ".",
+          (matrix[i] ?? "")[j] ?? ".",
+          (matrix[i] ?? "")[j + 1] ?? ".",
+          (matrix[i + 1] ?? "")[j - 1] ?? ".",
+          (matrix[i + 1] ?? "")[j] ?? ".",
+          (matrix[i + 1] ?? "")[j + 1] ?? ".",
+        ];
+        console.log("");
+        console.log("surrounding: ", surrounding);
+        console.log("");
+        // Check if any of the surrounding characters are not numbers
+        if (surrounding.some((x) => /[^0-9.]/.test(x))) {
+          result += parseInt(match[0]);
+          break;
         }
       }
     }
   }
 
-  console.log("result: ", result);
+  console.log(result);
   return result;
 };
 
-const expectedFirstSolution = 4361;
+const expectedFirstSolution = 525181;
 
 const second = (input: string) => {
   let startIdx = 0;
